ed4ec4330a2618f13c8c341542cbad49b7346d9e
cocoared <dmm23program@gmail.com>
2022-02-25 15:17:04 +0000

    [Fix]application_contrroller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,28 @@
 class BooksController < ApplicationController
 
+  def new
+    @book = Book.new
+  end
+
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def create
+    @user = current_user
+    @books = Book.all
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
-      @books = Book.all
-      render 'index'
+      render "index"
     end
   end
 
@@ -40,6 +48,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
